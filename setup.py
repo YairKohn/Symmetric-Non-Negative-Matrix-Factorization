9efb356095bc70df84a8cce2e1e350882e8e1962
YairKohn <talkohn68@gmail.com>
2025-08-12 16:49:35 +0300

    change setup

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup, Extension
 module = Extension(
     'symnmf',
     sources=['symnmfmodule.c', 'symnmf.c'],
-    extra_compile_args=['-ansi', '-Wall', '-Wextra', '-Werror', '-pedantic-errors'],
-    extra_link_args=['-lm']
 )
 
 setup(
