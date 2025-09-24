9c5f2a975443b6070298af9aab76673db4a57fd5
hilitzohar <hilitzohar@mail.tau.ac.il>
2025-09-24 10:59:22 +0300

    back to old version

--- Prev_final_100/symnmf.h
+++ Prev_final_100/symnmf.h
@@ -5,15 +5,15 @@ int allocate_2D_array(double ***arr, int rows, int cols);
 
 int free_2D_array(double ***arr, int rows);
 
-int C_sym(double ***A, double ***X, const int N, const int d);
+int C_sym(double ***A, double ***X, const int rows, const int cols);
 
 int C_ddg(double **D, double ***A, const int N);
 
-int C_norm(double ***W, const double *D, const double * const *A, const int N);
+int C_norm(double ***W, double **D, double ***A, const int N);
 
 int C_symnmf(double ***H_out, double ***H_in, const int rows_H, const int cols_H,
              double ***W, const int N_W);
 
-int parse_diag_to_matrix_form(double ***M, const double *D, const int N);
+int parse_diag_to_matrix_form(double ***M, double **D, const int N);
 
 #endif
