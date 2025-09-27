dc041260e68f23c12152f20f3d27c64750afe8d2
hilitzohar <hilitzohar@tau.mail.ac.il>
2025-09-27 07:21:03 +0000

    doco

--- kmeans.py
+++ kmeans.py
@@ -1,48 +1,68 @@
 import sys
 import math
 
-
-def print_error_and_exit():
-    print("An Error Has Occurred")
-    sys.exit(1)
-
-
-def read_input():
-    points = []
-    for line in sys.stdin:
-        line = line.strip() #delete leading and trailing whitespace
-        if not line:
-            continue
-        vector = list(map(float, line.split(',')))
-        points.append(vector)
-    return points
+# def read_input():
+#     points = []
+#     for line in sys.stdin:
+#         line = line.strip() #delete leading and trailing whitespace
+#         if not line:
+#             continue
+#         vector = list(map(float, line.split(',')))
+#         points.append(vector)
+#     return points
 
 def euclidean_distance(p1, p2):
+    """
+    Calculates the Euclidean distance between two points.
+    :param p1: The first point.
+    :type p1: list
+    :param p2: The second point.
+    :type p2: list
+    :return: The Euclidean distance between the two points.
+    :rtype: float
+    """
     return math.sqrt(sum((a - b) ** 2 for a, b in zip(p1, p2)))
 
 def mean(points):
+    """
+    Calculates the mean of a list of points.
+    The centroid calculation step:
+    compute the mean of all points assigned to a cluster to update the cluster centroid.
+    :param points: The list of points.
+    :type points: list of lists
+    :return: The mean of the points.
+    :rtype: list
+    """
     d = len(points[0])
     n = len(points)
     return [sum(point[i] for point in points) / n for i in range(d)]
 
 def kmeans(data, K, max_iter=400 ):
-    epsilon=1e-4
+    """ 
+    Runs the k-means clustering algorithm.
+    :param data: The data points.
+    :type data: list of lists
+    :param K: The number of clusters.
+    :type K: int
+    :param max_iter: The maximum number of iterations.
+    :type max_iter: int
+    :return: The centroids.
+    :rtype: list of lists
+    """
+    epsilon = 1e-4
     centroids = [data[i][:] for i in range(K)] # Initialize centroids with the first K points, doing shallow copies.
     for _ in range(max_iter):
         clusters = [[] for _ in range(K)]
 
-        # 3.Assign points to the nearest centroid
-        for point in data:
+        for point in data: # Assign points to the nearest centroid
             distances = [euclidean_distance(point, centroid) for centroid in centroids]
             min_index = distances.index(min(distances))
             clusters[min_index].append(point)
 
         converged = True
         new_centroids = []
-
-        # 4.Update centroids
-        for i in range(K):
-            if clusters[i]:  # Avoid empty cluster
+        for i in range(K): # Update centroids
+            if clusters[i]: # Avoid empty cluster
                 new_centroid = mean(clusters[i])
             else:
                 new_centroid = centroids[i]  # Keep the old centroid if cluster is empty
@@ -57,58 +77,4 @@ def kmeans(data, K, max_iter=400 ):
 
         if converged:
             break
-
     return centroids
-
-# Example usage
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("K", type=str, help="Number of clusters (1 < K < N)")
-    parser.add_argument("ITER", type=str, nargs='?', default="400", help="Max number of iterations (1 < ITER < 1000)")
-
-    args = parser.parse_args()
-
-    try:
-        args.K = float(args.K)
-    except ValueError:
-        print("Incorrect number of clusters!")
-        sys.exit(1)
-    try:
-        args.ITER = float(args.ITER)
-    except ValueError:
-        print("Incorrect maximum iteration!")
-        sys.exit(1)    
-    
-
-    
-    if not int(args.K) == args.K:
-        print("Incorrect number of clusters!")
-        sys.exit(1)
-    if not int(args.ITER) == args.ITER:
-        print("Incorrect maximum iteration!")
-        sys.exit(1)
-
-    K = int(args.K)
-    ITER = int(args.ITER)
-
-    data = read_input()
-
-    if not (1 < K < len(data)):
-        print("Incorrect number of clusters!")
-        sys.exit(1)
-    if not (1 < ITER < 1000):
-        print("Incorrect maximum iteration!")
-        sys.exit(1)
-
-    # After reading data
-    if len(data) > 0:
-        dim = len(data[0])
-        for i, vec in enumerate(data):
-            if len(vec) != dim:
-                print_error_and_exit()
-
-    centroids = kmeans(data, K, ITER)
-    for centroid in centroids:
-        print(",".join("%.4f" % x for x in centroid))
